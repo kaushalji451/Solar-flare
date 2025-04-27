a75fb1ae47013b538dd242c5b7b9dff7fcd00599
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-04-27 22:52:35 +0530

    doone

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/urboncompany";
+const MONGO_URL = process.env.MONGO_URI;
 main()
   .then(() => {
     console.log("connected to DB");
