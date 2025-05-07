dcbee4f44b0b9b83a468de72bfadc93976df8837
abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-05-07 15:22:57 +0530

    done

--- backend/initilizedb/index.js
+++ backend/initilizedb/index.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const Product = require("../models/product");
 const initdata = require("./data");
+require('dotenv').config();
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/urboncompany";
+
+const MONGO_URL = process.env.MONGO_URI;
 main()
   .then(() => {
     console.log("connected to DB");
